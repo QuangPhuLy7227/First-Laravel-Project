6682bcd86de1546a07411598ec5037f1058d908b
Quang Phu Ly <quangphuly150402@gmail.com>
2024-05-26 00:26:25 -0400

    Add Forms to create, update, and delete jobs

--- routes/web.php
+++ routes/web.php
@@ -9,17 +9,79 @@ Route::get('/', function () {
     return view('home');
 });
 
+//Job Index
 Route::get('/jobs', function () {
-    $job = Job::with('employer')->paginate(3);
-    return view('jobs', [
+    $job = Job::with('employer')->latest()->simplePaginate(3);
+    return view('jobs.index', [
         'jobs' => $job
     ]);
 });
 
+//Job Create
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
+});
+
+//Job Show
 Route::get('/jobs/{id}', function ($id) {
 
     $job = Job::find( $id );
-    return view('job', ['job' => $job]);
+    return view('jobs.show', ['job' => $job]);
+});
+
+//Job Store
+Route::post('/jobs', function () {
+    //validation
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary'=> ['required'],
+    ]);
+
+    Job::create( [
+        'title'=> request('title'),
+        'salary'=> request('salary'),
+        'employer_id'=> 1
+    ]);
+
+    return redirect('/jobs');
+});
+
+//Job Edit
+Route::get('/jobs/{id}/edit', function ($id) {
+    return view('jobs.edit', [
+        'job'=> Job::find($id),
+    ]);
+});
+
+//Job Update
+Route::patch('/jobs/{id}', function ($id) {
+    //validate
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary'=> ['required'],
+    ]);
+    //authorize
+    //update the job
+    $job = Job::findOrFail( $id );
+    // $job->title = request('title');
+    // $job->salary = request('salary');
+    // $job->save();
+
+    $job->update([
+        'title'=> request('title'),
+        'salary'=> request('salary'),
+    ]);
+    //redirect to the job page
+    return redirect('/jobs/' . $job->id);
+});
+
+//Job Destroy
+Route::delete('/jobs/{id}', function ($id) {
+    //authorize
+    //delete the job
+    Job::findOrFail( $id )->delete();
+    //redirect
+    return redirect('/jobs');
 });
 
 Route::get('/contact', function () {
